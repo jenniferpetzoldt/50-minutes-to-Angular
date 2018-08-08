8f50369a151e869e49eb838d1f5c56073c5d171a
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-08 13:38:24 -0500

    two way data binding

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,11 @@ let foodApp = angular.module('foodApp', []);
 
 foodApp.controller('FoodController', function(){
     let vm = this;
+    vm.food = [];
 
-});
+    vm.addFood = function(){
+        console.log('in addFood', vm.foodIn);
+        vm.food.push(vm.foodIn);
+        console.log('food thus far:', vm.food);
+    }//end addFood
+});//end controller
